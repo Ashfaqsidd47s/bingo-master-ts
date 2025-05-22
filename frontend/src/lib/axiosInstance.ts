7b70e9e20e9016d22d46c086cd8191f251ebde7f
Ashfaqsidd47s <ashfaqsidd47@gmail.com>
2025-05-23 00:34:28 +0530

    fixed the api url

--- frontend/src/lib/axiosInstance.ts
+++ frontend/src/lib/axiosInstance.ts
@@ -3,7 +3,7 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || 'https://bingo-master-ts.vercel.app',
+  baseURL: import.meta.env.VITE_API_BASE_URL || 'https://bingo-master-ts.onrender.com',
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
